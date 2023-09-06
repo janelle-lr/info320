6ef5404152d00f85b52f04fdff54ddc0c9d49b23
Kris00123 <110203207+Kris00123@users.noreply.github.com>
2023-09-06 18:50:12 +1200

    Intial landing for form

---
+++ src/Reqform.js
@@ -0,0 +1,25 @@
+import './Reqform.css';
+import React from 'react';
+import logo from './contactLogo.png';
+
+const Form = () => {  
+  
+      return (
+          <div className="form">
+              <div className="form-sub">
+                  <div className="header">
+                      {/* <img className="image" src="320/src/contactLogo.png"/> */}
+                      <img src={logo} className="image" />
+  
+                  </div>
+
+                  </div>
+        </div>
+    );
+};
+
+
+
+export default Form;
+
+
