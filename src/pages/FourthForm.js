3421e25c45c55beaf4768f4e948970ff3c62d54c
Kris00123 <110203207+Kris00123@users.noreply.github.com>
2023-09-20 16:41:45 +1200

    form validation on intialForm page

--- src/pages/FourthForm.js
+++ src/pages/FourthForm.js
@@ -1,4 +1,127 @@
-import React from 'react';
+// import React from 'react';
+// import './intialForm.css';
+// import { Link } from "react-router-dom";
+
+
+// // Components
+// import "../components/Layout.css";
+// import "../components/Texts.css";
+// import NavigationBar from "../components/NavigationBar";
+// import Button from '../components/Button';
+
+
+// export const Formpage4 = () => {
+//   const backButtonClick = () => {
+//     // Add your desired functionality here, e.g., go back in history
+//     window.history.back();
+//   };
+//   return (
+//     <div className="form">
+//       <div className="form-v">
+//         < NavigationBar/>
+//         <div className="form-title"> MARK YOURS </div>
+        
+//         <div className="firstName">
+//           <label htmlFor="firstName">
+//           </label>
+//           <input
+//             type="text"
+//             id="firstName"
+//             name="firstName"
+//             className="half-width"
+//             placeholder="First Name"
+//             />
+//         </div>
+        
+//         <div className="text-input">
+//           <label htmlFor="lastName">
+//           </label>
+//           <input
+//             type="text"
+//             id="lastName"
+//             name="lastName"
+//             className="last-name"
+//             placeholder="Last Name"
+//           />
+//         </div>
+
+//         <div className="organization">
+//           <label htmlFor="organization" >
+//           </label>
+//           <input
+//             type="text"
+//             id="organization"
+//             name="organization"
+//             className="full-width"
+//             placeholder="Enter your organization"
+//           />
+//         </div>
+//         <div className="address">
+//           <label htmlFor="address">
+//           </label>
+//           <input
+//             type="text"
+//             id="address"
+//             name="address"
+//             className="full-width"
+//             placeholder="Enter your address"
+//           />
+//         </div>
+        
+//         <div className="email">
+//           <label htmlFor="email">
+//           </label>
+//           <input
+//             type="email"
+//             id="email"
+//             name="email"
+//             className="email-width"
+//             placeholder="Enter your email"
+//           />
+//         </div>
+//         <div className="cell-number">
+//           <label htmlFor="cellPhone">
+//           </label>
+//           <input
+//             type="number"
+//             id="cellPhone"
+//             name="cellPhone"
+//             className="cell-width"
+//             placeholder="Enter your cell phone number"
+//           />
+//         </div>
+
+//         {/* <div className="Next-button">
+//           <div className="overlap-group">
+//             <button type='button' className="Next"> Next</button>
+//           </div>
+//         </div>  */}
+
+//         <div className="Button">
+//           <Button variant="button-primary">
+//               <Link to="/FifthForm">
+//               Next
+//              </Link>
+//           </Button>
+//         </div>
+
+
+
+
+//         <div className="Cancel-button">
+//           <div className="div-wrapper">
+//             <div className="cancel-text" onClick={backButtonClick}>Back</div>
+//           </div>
+//         </div>
+//         <div className="page-no" >5 of 7</div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Formpage4;
+
+import React, { useState } from 'react';
 import './intialForm.css';
 import { Link } from "react-router-dom";
 
@@ -6,22 +129,96 @@ import { Link } from "react-router-dom";
 // Components
 import "../components/Layout.css";
 import "../components/Texts.css";
-import NavigationBar from "../components/NavigationBar";
+import NavigationBarForms from "../components/NavivagtionBarForms.js";
 import Button from '../components/Button';
 
+function MyForm() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lname: '',
+    email: '',
+    password: '',
+  });
 
-export const Formpage4 = () => {
-  const backButtonClick = () => {
-    // Add your desired functionality here, e.g., go back in history
-    window.history.back();
+  const [errors, setErrors] = useState({});
+  const [submittedData, setSubmittedData] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validationErrors = validateForm();
+
+    if (Object.keys(validationErrors).length === 0) {
+      // Form is valid, save the data
+      setSubmittedData(formData);
+    } else {
+      // Form is invalid, display errors
+      setErrors(validationErrors);
+    }
+  };
+
+  const validateForm = () => {
+    // Implement your form validation logic here
+    const errors = {};
+    if (!formData.firstName.trim()) {
+      errors.firstName = 'first name is required';
+    }
+    if (!formData.lname.trim()) {
+      errors.lname = 'Last Name is required';
+    }
+    if (!formData.email.trim()) {
+      errors.email = 'Email is required';
+    } else if (!isValidEmail(formData.email)) {
+      errors.email = 'Invalid email address';
+    }
+    if (!formData.password.trim()) {
+      errors.password = 'Password is required';
+    }
+    return errors;
+  };
+
+  const isValidEmail = (email) => {
+    // Implement email validation logic (e.g., regex)
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  };
+
   return (
-    <div className="form">
-      <div className="form-v">
-        < NavigationBar/>
-        <div className="form-title"> MARK YOURS </div>
+    <div>
+      {submittedData ? (
+        <div>
+          <h2>Form Submitted Successfully!</h2>
+          <p>First Name: {submittedData.firstName}</p>
+          <p>Last Name: {submittedData.lname}</p>
+          <p>Email: {submittedData.email}</p>
+          <p>Password: {submittedData.password}</p>
+
+        </div>
+      ) : (
+
         
-        <div className="firstName">
+        <form onSubmit={handleSubmit}>
+          <NavigationBarForms/>
+          <div className="firstName">            
+          <label htmlFor="firstName">First Name:</label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              className="half-width"
+              value={formData.firstName}
+              onChange={handleChange}
+            />
+            {errors.firstName && <p className="error">{errors.firstName}</p>}
+          </div>
+
+          {/* <div className="firstName">
           <label htmlFor="firstName">
           </label>
           <input
@@ -31,92 +228,50 @@ export const Formpage4 = () => {
             className="half-width"
             placeholder="First Name"
             />
-        </div>
-        
-        <div className="text-input">
-          <label htmlFor="lastName">
-          </label>
-          <input
-            type="text"
-            id="lastName"
-            name="lastName"
-            className="last-name"
-            placeholder="Last Name"
-          />
-        </div>
-
-        <div className="organization">
-          <label htmlFor="organization" >
-          </label>
-          <input
-            type="text"
-            id="organization"
-            name="organization"
-            className="full-width"
-            placeholder="Enter your organization"
-          />
-        </div>
-        <div className="address">
-          <label htmlFor="address">
-          </label>
-          <input
-            type="text"
-            id="address"
-            name="address"
-            className="full-width"
-            placeholder="Enter your address"
-          />
-        </div>
-        
-        <div className="email">
-          <label htmlFor="email">
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className="email-width"
-            placeholder="Enter your email"
-          />
-        </div>
-        <div className="cell-number">
-          <label htmlFor="cellPhone">
-          </label>
-          <input
-            type="number"
-            id="cellPhone"
-            name="cellPhone"
-            className="cell-width"
-            placeholder="Enter your cell phone number"
-          />
-        </div>
-
-        {/* <div className="Next-button">
-          <div className="overlap-group">
-            <button type='button' className="Next"> Next</button>
-          </div>
         </div>  */}
 
-        <div className="Button">
-          <Button variant="button-primary">
-              <Link to="/FifthForm">
-              Next
-             </Link>
-          </Button>
-        </div>
-
-
-
-
-        <div className="Cancel-button">
-          <div className="div-wrapper">
-            <div className="cancel-text" onClick={backButtonClick}>Back</div>
+          <div>
+            <label htmlFor="lname">Last Name:</label>
+            <input
+              type="text"
+              id="lname"
+              name="lname"
+              value={formData.lname}
+              onChange={handleChange}
+            />
+            {errors.lname && <p className="error">{errors.lname}</p>}
           </div>
-        </div>
-        <div className="page-no" >5 of 7</div>
-      </div>
+
+
+
+
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+            {errors.email && <p className="error">{errors.email}</p>}
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+            {errors.password && <p className="error">{errors.password}</p>}
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      )}
     </div>
   );
-};
+}
 
-export default Formpage4;
+export default MyForm;
--- src/pages/intialForm.js
+++ src/pages/intialForm.js
@@ -1,119 +1,318 @@
-import React from 'react';
-import './intialForm.css';
-import { Link } from "react-router-dom";
+// import React from 'react';
+// import './intialForm.css';
+// import { Link } from "react-router-dom";
 
+
+// // Components
+// import "../components/Layout.css";
+// import "../components/Texts.css";
+// import NavigationBar from "../components/NavigationBar";
+// import Button from '../components/Button';
+
+
+// export const Formpage1 = () => {
+//   const backButtonClick = () => {
+//     // Add your desired functionality here, e.g., go back in history
+//     window.history.back();
+//   };
+//   return (
+//     <div className="form">
+//       <div className="form-v">
+//         < NavigationBar/>
+//         <div className="form-title">Applicant Details</div>
+        
+//         <div className="firstName">
+//           <label htmlFor="firstName">
+//           </label>
+//           <input
+//             type="text"
+//             id="firstName"
+//             name="firstName"
+//             className="half-width"
+//             placeholder="First Name"
+//             />
+//         </div>
+        
+//         <div className="text-input">
+//           <label htmlFor="lastName">
+//           </label>
+//           <input
+//             type="text"
+//             id="lastName"
+//             name="lastName"
+//             className="last-name"
+//             placeholder="Last Name"
+//           />
+//         </div>
+
+//         <div className="organization">
+//           <label htmlFor="organization" >
+//           </label>
+//           <input
+//             type="text"
+//             id="organization"
+//             name="organization"
+//             className="full-width"
+//             placeholder="Enter your organization"
+//           />
+//         </div>
+//         <div className="address">
+//           <label htmlFor="address">
+//           </label>
+//           <input
+//             type="text"
+//             id="address"
+//             name="address"
+//             className="full-width"
+//             placeholder="Enter your address"
+//           />
+//         </div>
+        
+//         <div className="email">
+//           <label htmlFor="email">
+//           </label>
+//           <input
+//             type="email"
+//             id="email"
+//             name="email"
+//             className="email-width"
+//             placeholder="Enter your email"
+//           />
+//         </div>
+//         <div className="cell-number">
+//           <label htmlFor="cellPhone">
+//           </label>
+//           <input
+//             type="number"
+//             id="cellPhone"
+//             name="cellPhone"
+//             className="cell-width"
+//             placeholder="Enter your cell phone number"
+//           />
+//         </div>
+
+//         {/* <div className="Next-button">
+//           <div className="overlap-group">
+//             <button type='button' className="Next"> Next</button>
+//           </div>
+//         </div>  */}
+
+//         <div className="Button">
+//           <Button variant="button-primary">
+//               <Link to="/SecondForm">
+//               Next
+//              </Link>
+//           </Button>
+//         </div>
+
+
+
+
+//         <div className="Cancel-button">
+//           <div className="div-wrapper">
+//             <div className="cancel-text" onClick={backButtonClick}>Back</div>
+//           </div>
+//         </div>
+//         <div className="page-no" >2 of 7</div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Formpage1;
+
+
+import React, { useState } from 'react';
+import './intialForm.css';
 
 // Components
 import "../components/Layout.css";
 import "../components/Texts.css";
-import NavigationBar from "../components/NavigationBar";
 import Button from '../components/Button';
-
+import FormButton from '../components/FormButtons.js';
+import NavigationBarForm from '../components/NavivagtionBarForms';
 
 export const Formpage1 = () => {
-  const backButtonClick = () => {
-    // Add your desired functionality here, e.g., go back in history
-    window.history.back();
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    organization: "",
+    address: "",
+    email: "",
+    cellPhone: ""
+  });
+
+  const [errors, setErrors] = useState({});
+
+// setting out the error validation prompts for each of the feilds where an input is required to be filled in 
+
+  const validateForm = () => {
+    const newErrors = {};
+    if (!formData.firstName) {
+      newErrors.firstName = "First name is required";
+    }
+    if (!formData.lastName) {
+      newErrors.lastName = "Last name is required";
+    }
+    if (!formData.organization) {
+      newErrors.organization = "Organization is required";
+    }
+    if (!formData.address) {
+      newErrors.address = "Address is required";
+    }
+    if (!formData.email) {
+      newErrors.email = "Email is required";
+    } else if (!/^\S+@\S+\.\S+$/.test(formData.email)) {
+      newErrors.email = "Invalid email format";
+    }
+    if (!formData.cellPhone) {
+      newErrors.cellPhone = "Cell phone number is required";
+    // } else if (!/^\d{20}$/.test(formData.cellPhone)) {
+    //   newErrors.cellPhone = "Invalid cell phone number format";
+    }
+    return newErrors;
   };
+
+
+  // const handleNextClick = () => {
+  //   const newErrors = validateForm();
+  //   if (Object.keys(newErrors).length === 0) {
+  //     // Validation passed, navigate to the next form page
+  //     // You can also submit the form data to your server here
+  //     // For now, let's print the data to the console
+  //     console.log(formData);
+  //   } else {
+  //     // Validation failed, set the errors state to display error messages
+  //     setErrors(newErrors);
+  //   }
+  // };
+
+// handleNextClick triggers the repsonse from the next button at the button 
+
+  const handleNextClick = () => {
+    const newErrors = validateForm();
+    if (Object.keys(newErrors).length === 0) {
+      // Validation passed, navigate to the next form page
+      // You can also submit the form data to your server here
+      // For now, let's print the data to the console
+      console.log(formData);
+  
+      // Reset the errors state to clear error messages
+      setErrors({});
+    } else {
+      // Validation failed, set the errors state to display error messages
+      setErrors(newErrors);
+    }
+  };
+  
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
     <div className="form">
       <div className="form-v">
-        < NavigationBar/>
+        <NavigationBarForm />
         <div className="form-title">Applicant Details</div>
-        
+
         <div className="firstName">
-          <label htmlFor="firstName">
-          </label>
+          <label htmlFor="firstName">First Name</label>
           <input
             type="text"
             id="firstName"
             name="firstName"
             className="half-width"
             placeholder="First Name"
-            />
+            value={formData.firstName}
+            onChange={handleChange}
+          />
         </div>
-        
+
         <div className="text-input">
-          <label htmlFor="lastName">
-          </label>
           <input
             type="text"
             id="lastName"
             name="lastName"
             className="last-name"
             placeholder="Last Name"
+            value={formData.lastName}
+            onChange={handleChange}
           />
+
+{/* All of the error messages are printing here - this is so that its easy to see in dev stage */}
+          {errors.firstName && <div className="error-message">{errors.firstName}</div>}
+          {errors.lastName && <div className="error-message">{errors.lastName}</div>}
+          {errors.organization && <div className="error-message">{errors.organization}</div>}
+          {errors.address && <div className="error-message">{errors.address}</div>}
+          {errors.email && <div className="error-message">{errors.email}</div>}
+          {errors.cellPhone && <div className="error-message">{errors.cellPhone}</div>}
+
         </div>
 
         <div className="organization">
-          <label htmlFor="organization" >
-          </label>
+          <label htmlFor="organization"></label>
           <input
             type="text"
             id="organization"
             name="organization"
             className="full-width"
-            placeholder="Enter your organization"
+            placeholder="Enter your organization name"
+            value={formData.organization}
+            onChange={handleChange}
           />
         </div>
+
         <div className="address">
-          <label htmlFor="address">
-          </label>
+          <label htmlFor="address"></label>
           <input
             type="text"
             id="address"
             name="address"
             className="full-width"
             placeholder="Enter your address"
+            value={formData.address}
+            onChange={handleChange}
           />
         </div>
-        
+
         <div className="email">
-          <label htmlFor="email">
-          </label>
+          <label htmlFor="email"></label>
           <input
             type="email"
             id="email"
             name="email"
             className="email-width"
             placeholder="Enter your email"
+            value={formData.email}
+            onChange={handleChange}
           />
         </div>
+
         <div className="cell-number">
-          <label htmlFor="cellPhone">
-          </label>
+          <label htmlFor="cellPhone"></label>
           <input
             type="number"
             id="cellPhone"
             name="cellPhone"
             className="cell-width"
             placeholder="Enter your cell phone number"
+            value={formData.cellPhone}
+            onChange={handleChange}
           />
         </div>
 
-        {/* <div className="Next-button">
-          <div className="overlap-group">
-            <button type='button' className="Next"> Next</button>
-          </div>
-        </div>  */}
+        {/* this part below is the button in terms of the validation - i have tried to 
+        enable it to the FormButton but my one doesnt work as of now, if you want to see the validation work uncomment below section! */}
 
         <div className="Button">
-          <Button variant="button-primary">
-              <Link to="/SecondForm">
-              Next
-             </Link>
+          <Button variant="button-primary" onClick={handleNextClick}>
+            Next
           </Button>
         </div>
 
-
-
-
-        <div className="Cancel-button">
-          <div className="div-wrapper">
-            <div className="cancel-text" onClick={backButtonClick}>Back</div>
-          </div>
-        </div>
-        <div className="page-no" >2 of 7</div>
+        {/* <FormButton  onClick={handleNextClick}  page="2" leftButton="Back" leftButtonDest="/SecondForm" rightButton="Next" rightButtonDest="/FourthForm" /> */}
       </div>
     </div>
   );
