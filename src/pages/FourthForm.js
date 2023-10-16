5f498226e03a5b5b208324f961c9915177106ac9
janelle-lr <janelle.limranola@gmail.com>
2023-10-16 14:15:40 +1300

    adding end data form validation

--- src/pages/FourthForm.js
+++ src/pages/FourthForm.js
@@ -21,34 +21,34 @@ export const Formpage4 = () => {
   const [errors, setErrors] = useState({});
 
   // setting out the error validation prompts for each of the feilds where an input is required to be filled in 
-    const validateForm = () => {
-      const newErrors = {};
-      if (!formData4.flowLevel) {
-        newErrors.flowLevel = "Flow level is required";
-      }
-      if (!formData4.startDateTime) {
-        newErrors.startDateTime = "Start date time is required";
-      }
-      if (!formData4.endDateTime) {
-        newErrors.endDateTime = "End date time is required";
-      }
-      if (!formData4.Dropdown) {
-        newErrors.Dropdown = "Area of request is required";
-      }
-      if (!formData4.Dropdown) {
-        newErrors.Dropdown = "Area of request is required";
-      }
-      return newErrors;
-    };
-    const currentDateTime = new Date().toISOString().slice(0, 16);
-  
+  const validateForm = () => {
+    const newErrors = {};
+    if (!formData4.flowLevel) {
+      newErrors.flowLevel = "Flow level is required";
+    }
+    if (!formData4.startDateTime) {
+      newErrors.startDateTime = "Start date time is required";
+    }
+    if (!formData4.endDateTime) {
+      newErrors.endDateTime = "End date time is required";
+    }
+    if (!formData4.Dropdown) {
+      newErrors.Dropdown = "Area of request is required";
+    }
+    if (!formData4.Dropdown) {
+      newErrors.Dropdown = "Area of request is required";
+    }
+    return newErrors;
+  };
+  const currentDateTime = new Date().toISOString().slice(0, 16);
+
   // nextClick triggers the repsonse from the next button at the button 
   const nextClick = () => {
     const newErrors = validateForm();
     if (Object.keys(newErrors).length === 0) {
       // Validation passed, navigate to the next form page
       console.log(formData4);
-  
+
       // Reset the errors state to clear error messages
       setErrors({});
       saveFormData4();
@@ -58,18 +58,18 @@ export const Formpage4 = () => {
       setErrors(newErrors);
     }
   };
-  
-    // Load saved form data from localStorage when the component mounts
-    useEffect(() => {
-      const savedFormData4 = JSON.parse(localStorage.getItem("formData4")) || {};
-      setFormData4(savedFormData4);
-    }, []); // The empty dependency array ensures this effect runs once when the component mounts
-  
+
+  // Load saved form data from localStorage when the component mounts
+  useEffect(() => {
+    const savedFormData4 = JSON.parse(localStorage.getItem("formData4")) || {};
+    setFormData4(savedFormData4);
+  }, []); // The empty dependency array ensures this effect runs once when the component mounts
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData4({ ...formData4, [name]: value });
   };
-  
+
   const saveFormData4 = () => {
     try {
       localStorage.setItem("formData4", JSON.stringify(formData4));
@@ -89,35 +89,35 @@ export const Formpage4 = () => {
   const divStyle = {
     display: isDivShow ? 'block' : 'none',
   };
-  
+
   return (
     <div className="request-form section-container">
       <div className="fourth-form main-content">
         <NavigationBarForm />
-        <div className="grey-out" id="grey-out"  style={divStyle}></div>
+        <div className="grey-out" id="grey-out" style={divStyle}></div>
         <div className="popup" style={divStyle}>
           <h3>Ideal Flows</h3>
-            <table className="flow-info">
-              <thead>
-                <tr>
-                  <th>Flow Level</th>
-                  <th>Activity</th>
-                  <th>Notes</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>30 - 50 m&sup3;/s</td>
-                  <td>River kayaking</td>
-                  <td>-</td>
-                </tr>
-                <tr>
-                  <td>64 m&sup3;/s</td>
-                  <td>Surfing</td>
-                  <td>A local favourite!</td>
-                </tr>
-              </tbody>
-            </table>
+          <table className="flow-info">
+            <thead>
+              <tr>
+                <th>Flow Level</th>
+                <th>Activity</th>
+                <th>Notes</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>30 - 50 m&sup3;/s</td>
+                <td>River kayaking</td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <td>64 m&sup3;/s</td>
+                <td>Surfing</td>
+                <td>A local favourite!</td>
+              </tr>
+            </tbody>
+          </table>
           <br />
           <Button variant="button-primary" onClick={toggleDiv}>Close</Button>
         </div>
@@ -128,7 +128,7 @@ export const Formpage4 = () => {
         <form className="container">
           <div className="label-invisible">
 
-          <label htmlFor="flowLevel">
+            <label htmlFor="flowLevel">
               Required flow/level range (e.g. 30 - 40 m&sup3;/s)
             </label>
             <input
@@ -142,32 +142,32 @@ export const Formpage4 = () => {
             />
             <Link className="" id="info" onClick={toggleDiv}>
               <img src={InformationIcon} className="info-icon" />
-            </Link>            
-            </div>
-            <div className="error-message">{errors.flowLevel}</div>
+            </Link>
+          </div>
+          <div className="error-message">{errors.flowLevel}</div>
 
-          <Dropdown 
-          name="Dropdown" 
-          value={formData4.Dropdown}
-          onChange={handleChange}
+          <Dropdown
+            name="Dropdown"
+            value={formData4.Dropdown}
+            onChange={handleChange}
           />
           {errors.Dropdown && (
-              <div className="error-message">{errors.Dropdown}</div>
+            <div className="error-message">{errors.Dropdown}</div>
           )}
-            
+
 
           <div className="row">
             <label htmlFor="startDateTime">Start date & time</label>
-            <input type="datetime-local" name="startDateTime" className="text-box six-twelfths" placeholder="Start date & time (DD/MM/YYYY 00:00)" 
-            min={currentDateTime}
-            value={formData4.startDateTime}
-            onChange={handleChange}
+            <input type="datetime-local" name="startDateTime" className="text-box six-twelfths" placeholder="Start date & time (DD/MM/YYYY 00:00)"
+              min={currentDateTime}
+              value={formData4.startDateTime}
+              onChange={handleChange}
             />
 
             <label htmlFor="endDateTime">End date & time</label>
-            <input type="datetime-local" name="endDateTime" className="text-box six-twelfths" placeholder="End date & time (DD/MM/YYYY 00:00)" 
-            value={formData4.endDateTime}
-            onChange={handleChange}
+            <input type="datetime-local" name="endDateTime" className="text-box six-twelfths" placeholder="End date & time (DD/MM/YYYY 00:00)"
+              min={currentDateTime} value={formData4.endDateTime}
+              onChange={handleChange}
             />
           </div>
 
@@ -177,7 +177,7 @@ export const Formpage4 = () => {
           </div>
         </form>
 
-        <BottomButtons  page="5" leftButton="Back" leftButtonDest="/ThirdForm" rightButton="Next" rightOnClick={nextClick} />
+        <BottomButtons page="5" leftButton="Back" leftButtonDest="/ThirdForm" rightButton="Next" rightOnClick={nextClick} />
 
       </div>
     </div>
